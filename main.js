c9151b776a0963baa298b49f036538ed46ff10e9
unknown <xydozhnikbomj@gmail.com>
2023-04-27 11:01:03 +0300

    1.0.0

---
+++ main.js
@@ -0,0 +1,8 @@
+firstNumber = +prompt('Enter some nomber?(1st)');
+secondNumber = +prompt('Enter some nomber?(2nd)');
+thirdNumber = +prompt('Enter some nomber?(3rd)');
+sum = firstNumber + secondNumber + thirdNumber;
+arithmAverege = sum/3; 
+alert(`
+        The arithmetic mean of the numbers : ${firstNumber} ${secondNumber} ${thirdNumber} 
+        This : ${arithmAverege} `) ;
